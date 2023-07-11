d9eef81c73d366cbbc0c6ae899c440c3956b7d6c
MKYRENE <rodriguezm90@icloud.com>
2023-07-10 21:33:08 -0400

    updated connection.js file and seeds.sql file with connection sql query for the app.

--- app.js
+++ app.js
@@ -1,23 +1,65 @@
 const inquirer = require('inquirer');
 const { viewAllDepartments, viewAllRoles, viewAllEmployees, addDepartment, addRoles, addEmployee, updateEmployeeRole } = require('./connection'); // creating the connection 
 
-//Function to prompt the unser for the desired action
+//This code sets up the initial prompt using Inquirer and handles the user's selected actions by calling the respective functions from the connection.js file.
+
+// Function to prompt the user for the desired action
 function promptUser() {
     inquirer
-        .prompt({
-            name: 'action',
-            type: 'list',
-            message: 'What would you like todo ?',
-            choices: [
-                'View all departments',
-                'View all roles',
-                'View all employees',
-                'Add a department',
-                'Add a role',
-                'Add an employee',
-                'Update an employee role',
-                'Exit'
-            ]
-
-        })
-}
+      .prompt({
+        name: 'action',
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: [
+          'View all departments',
+          'View all roles',
+          'View all employees',
+          'Add a department',
+          'Add a role',
+          'Add an employee',
+          'Update an employee role',
+          'Exit'
+        ],
+      })
+      .then((answer) => {
+        switch (answer.action) {
+          case 'View all departments':
+            viewAllDepartments();
+            break;
+          case 'View all roles':
+            viewAllRoles();
+            break;
+          case 'View all employees':
+            viewAllEmployees();
+            break;
+          case 'Add a department':
+            addDepartment();
+            break;
+          case 'Add a role':
+            addRole();
+            break;
+          case 'Add an employee':
+            addEmployee();
+            break;
+          case 'Update an employee role':
+            updateEmployeeRole();
+            break;
+          case 'Exit':
+            console.log('Exiting Employee Tracker...');
+            connection.end();
+            process.exit(0);
+          default:
+            console.log('Invalid action.');
+            promptUser();
+        }
+      });
+  }
+  
+  //Function to start the app
+  function startApp() {
+    console.log('Welcome to Employee Tracker!');
+    promptUser();
+  }
+  
+  // Starting point of the application
+  startApp();
---
+++ db/connection .js
@@ -0,0 +1,30 @@
+const mysql = require('mysql2');
+
+//Creating a connection to the database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3000,
+    user: 'root',
+    password: '',
+    database: 'employee_db',
+
+});
+
+// Function to view all departments
+function viewAllDepartments() {
+    connection.query('SELECT * FROM department', (err, results) => {
+      if (err) throw err;
+      console.table(results);
+      promptUser();
+    });
+  }
+  
+  // Export the function
+  module.exports = {
+    viewAllDepartments,
+    // Other database query functions...
+  };
+  
+
+//**********COMMENTS/NOTES FOR MYSELF TO REFERENCE ********
+//Update the host, port, user, password, and database values according to your MySQL configuration. This code establishes the connection to the database and exports the connection object for use in other files.
