960d8c305893e44b34336366e51ae98e32a4313c
MKYRENE <rodriguezm90@icloud.com>
2023-07-11 01:09:11 -0400

    updated create_table sql file with employee db database.

--- db/create_table.sql
+++ db/create_table.sql
@@ -1,3 +1,11 @@
+-- Drop or delete the database to reset it
+DROP DATABASE IF EXISTS employee_db;
+
+-- Create our employee_db database
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
 -- Create department table
 CREATE TABLE department (
   id INT PRIMARY KEY,
@@ -7,8 +15,8 @@ CREATE TABLE department (
 -- Create role table
 CREATE TABLE role (
   id INT PRIMARY KEY,
-  title VARCHAR(30),
-  salary DECIMAL,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
   department_id INT,
   FOREIGN KEY (department_id) REFERENCES department(id)
 );
@@ -16,8 +24,8 @@ CREATE TABLE role (
 -- Create employee table
 CREATE TABLE employee (
   id INT PRIMARY KEY,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
   role_id INT,
   manager_id INT,
   FOREIGN KEY (role_id) REFERENCES role(id),
